75d88d266bc312bb56a0745251203d5b85863579
ElyasAmri <elyas.amri@gmail.com>
2022-04-26 14:01:17 +0300

    chore: remove src/index.js

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Switch } from "react-router-dom";
-
-ReactDOM.render(<App />, document.getElementById("root"));
