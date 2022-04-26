cb870927387545ed7ecde95a3b240d96f5c32580
ElyasAmri <elyas.amri@gmail.com>
2022-04-26 14:00:44 +0300

    chore: move main imports to _app

---
+++ pages/_app.js
@@ -0,0 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+import './assets/css/main.css'
+import 'aos/dist/aos.css'
+
+function MyApp({Component, pageProps}) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
